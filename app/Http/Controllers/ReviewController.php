18ec0b81ab41aa4a9abe55cc34a05d8994143b91
henry <henryonyemaobi@gmail.com>
2020-04-09 17:14:44 +0100

    review GET API created

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\review;
+use App\Http\Resources\ReviewResource as ReviewRes;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+       /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //View all reviews
+        $reviews = review::orderBy('id','desc')
+         ->select('id','name','url','note','created_at')
+         ->paginate(6);
+
+        return ReviewRes::collection($reviews);
+    }
 }
