568813d369169c73c5153e2ac0be43e7037e6172
henry <henryonyemaobi@gmail.com>
2020-04-10 00:07:48 +0100

    corrected blog resource typo

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\blog;
-use App\Http\Resources\blogResource as BlogRes;
+use App\Http\Resources\BlogResource as BlogRes;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
